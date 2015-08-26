c645861d15b21b6d98336391797208d3e6cd41c2
Ken Elkabany <kelkabany@dropbox.com>
2015-08-26 13:09:34 -0700

    Bump to 3.21.

--- dropbox/dropbox.py
+++ dropbox/dropbox.py
@@ -3,7 +3,7 @@ __all__ = [
 ]
 
 # TODO(kelkabany): We need to auto populate this as done in the v1 SDK.
-__version__ = '3.2'
+__version__ = '3.21'
 
 import json
 import logging
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open('LICENSE') as f:
 
 dist = setup(
     name='dropbox',
-    version='3.2',
+    version='3.21',
     description='Official Dropbox API Client',
     author='Dropbox',
     author_email='dev-platform@dropbox.com',
