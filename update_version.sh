4bc887d1a954d9eb8cfe88ccd8e408962c386655
Ken Elkabany <kelkabany@dropbox.com>
2016-08-10 16:27:51 -0700

    updated_version.sh prints the current version if no args are given.

--- update_version.sh
+++ update_version.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-if [ -z $1 ]; then echo "error: $0 needs a version number as argument."; fi
-sed -i "s/^__version__.*/__version__ = '$1'/g" dropbox/dropbox.py
+if [ -z $1 ]; then
+    echo "error: $0 needs a version number as argument. Current version: `python -c 'import dropbox; print(dropbox.__version__)'`";
+else
+    sed -i "s/^__version__.*/__version__ = '$1'/g" dropbox/dropbox.py
+fi
