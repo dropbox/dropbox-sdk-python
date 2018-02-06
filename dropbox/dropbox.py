e30886f7d49076a81e4ea40426060a1a40d8c4f0
rob <robarmstrong@dropbox.com>
2018-02-05 21:01:21 -0800

    fix regression for as_user() and as_admin()

--- dropbox/dropbox.py
+++ dropbox/dropbox.py
@@ -596,4 +596,12 @@ class DropboxTeam(_DropboxTransport, DropboxTeamBase):
 
         new_headers = self._headers.copy() if self._headers else {}
         new_headers[select_header_name] = team_member_id
-        return self.clone(headers=new_headers)
+        return Dropbox(
+            self._oauth2_access_token,
+            max_retries_on_error=self._max_retries_on_error,
+            max_retries_on_rate_limit=self._max_retries_on_rate_limit,
+            timeout=self._timeout,
+            user_agent=self._raw_user_agent,
+            session=self._session,
+            headers=new_headers,
+        )
