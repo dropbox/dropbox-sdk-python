550c294fb97eb063c9927c36a2ad4e5d60a8d0fd
Greg <greg-db@users.noreply.github.com>
2022-06-30 13:17:15 -0400

    Address lint issues and typo

--- example/performant_upload.py
+++ example/performant_upload.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-"""Example code for uploading a local folder of files to Dropbox using the Dropbox API in a performant manner."""
+"""Example code for uploading a local folder of files to Dropbox using the Dropbox API in a
+performant manner."""
 
 from concurrent.futures import ThreadPoolExecutor
 
@@ -16,25 +17,36 @@ logging.getLogger().setLevel(logging.INFO)
 
 MB = 1024 * 1024
 
-parser = argparse.ArgumentParser(description='Upload a folder of files to Dropbox in a performant manner.')
-parser.add_argument('--local_path', required=True, help='Local path of folder of files to upload (non-recursive).')
-parser.add_argument('--remote_path', required=True, help='Remote path of folder in Dropbox to upload to.')
-parser.add_argument('--access_token', help='Access token to use to perform the API calls.')
-parser.add_argument('--refresh_token', help='Refresh token to use to retrieve access tokens.')
-parser.add_argument('--app_key', help='App key to use to retrieve access tokens. Required when using a refresh token.')
-parser.add_argument('--app_secret', help='App secret to use to retrieve access tokens.')
+parser = argparse.ArgumentParser(
+    description='Upload a folder of files to Dropbox in a performant manner.')
+parser.add_argument('--local_path', required=True,
+    help='Local path of folder of files to upload (non-recursive).')
+parser.add_argument('--remote_path', required=True,
+    help='Remote path of folder in Dropbox to upload to.')
+parser.add_argument('--access_token',
+    help='Access token to use to perform the API calls.')
+parser.add_argument('--refresh_token',
+    help='Refresh token to use to retrieve access tokens.')
+parser.add_argument('--app_key',
+    help='App key to use to retrieve access tokens. Required when using a refresh token.')
+parser.add_argument('--app_secret',
+    help='App secret to use to retrieve access tokens.')
 
-# These three values can be tuned for better (or worse) overall performance, based on the scenario and environment:
-parser.add_argument('--chunk_size', default=24*MB, help='The amount of data, in bytes, to send per upload request.')
-parser.add_argument('--batch_thread_count', default=20, help='How many threads to use per batch of upload sessions.')
-parser.add_argument('--concurrent_thread_count', default=10, help='How many threads to use per upload session.')
+# These three values can be tuned for better (or worse) overall performance,
+# based on the scenario and environment:
+parser.add_argument('--chunk_size', default=24 * MB,
+    help='The amount of data, in bytes, to send per upload request.')
+parser.add_argument('--batch_thread_count', default=20,
+    help='How many threads to use per batch of upload sessions.')
+parser.add_argument('--concurrent_thread_count', default=10,
+    help='How many threads to use per upload session.')
 
 
 def get_client(args):
     """Returns the Dropbox client to use to upload files."""
 
     if not (args.access_token or args.refresh_token):
-        raise Exception("Either an access token or refresh token/app is key required.")
+        raise Exception("Either an access token or refresh token/app key is required.")
 
     if args.refresh_token:
         if not args.app_key:
@@ -42,7 +54,8 @@ def get_client(args):
         return dropbox.Dropbox(
             oauth2_refresh_token=args.refresh_token,
             app_key=args.app_key,
-            app_secret=args.app_secret  # the app secret is required for refresh tokens not acquired using PKCE
+            # the app secret is required for refresh tokens not acquired using PKCE
+            app_secret=args.app_secret
         )
 
     return dropbox.Dropbox(oauth2_access_token=args.access_token)
@@ -82,21 +95,25 @@ def upload_session_appends(client, session_id, source_file_path, args):
         file_size = os.path.getsize(source_file_path)
 
         def append(dest_file_name, data, cursor, close):
-            logging.debug(f"Appending to upload session with ID '{cursor.session_id}' for file '{dest_file_name}'"
+            logging.debug(f"Appending to upload session with ID '{cursor.session_id}'"
+                          f" for file '{dest_file_name}'"
                           f" at offset: {str(cursor.offset)}")
             client.files_upload_session_append_v2(f=data,
                                                   cursor=cursor,
                                                   close=close)
-            logging.debug(f"Done appending to upload session with ID '{cursor.session_id}' for file '{dest_file_name}'"
+            logging.debug(f"Done appending to upload session with ID '{cursor.session_id}'"
+                          f" for file '{dest_file_name}'"
                           f" at offset: {str(cursor.offset)}")
 
         if file_size > 0:  # For non-empty files, start a number of concurrent append calls.
             with ThreadPoolExecutor(max_workers=args.concurrent_thread_count) as session_executor:
                 while local_file.tell() < file_size:
-                    cursor = dropbox.files.UploadSessionCursor(session_id=session_id, offset=local_file.tell())
+                    cursor = dropbox.files.UploadSessionCursor(
+                        session_id=session_id, offset=local_file.tell())
                     data = local_file.read(args.chunk_size)
                     close = local_file.tell() == file_size
-                    futures.append(session_executor.submit(append, dest_file_name, data, cursor, close))
+                    futures.append(
+                        session_executor.submit(append, dest_file_name, data, cursor, close))
         else:  # For empty files, just call append once to close the upload session.
             cursor = dropbox.files.UploadSessionCursor(session_id=session_id, offset=0)
             append(dest_file_name=dest_file_name, data=None, cursor=cursor, close=True)
@@ -109,7 +126,8 @@ def upload_session_appends(client, session_id, source_file_path, args):
                 raise append_exception
 
         return dropbox.files.UploadSessionFinishArg(
-            cursor=dropbox.files.UploadSessionCursor(session_id=session_id, offset=local_file.tell()),
+            cursor=dropbox.files.UploadSessionCursor(
+                session_id=session_id, offset=local_file.tell()),
             commit=dropbox.files.CommitInfo(path=f"/{dest_folder}/{dest_file_name}"))
 
 
@@ -121,7 +139,8 @@ def upload_files(client, files, args):
     uploaded_size = 0
 
     assert len(entries) <= 1000, "Max batch size is 1000."
-    assert args.chunk_size % (4 * MB) == 0, "Chunk size must be a multiple of 4 MB to use concurrent upload sessions"
+    assert args.chunk_size % (4 * MB) == 0, \
+        "Chunk size must be a multiple of 4 MB to use concurrent upload sessions"
 
     logging.info(f"Starting batch of {str(len(files))} upload sessions.")
     start_batch_result = client.files_upload_session_start_batch(
@@ -131,7 +150,10 @@ def upload_files(client, files, args):
     with ThreadPoolExecutor(max_workers=args.batch_thread_count) as batch_executor:
         for index, file in enumerate(files):
             futures.append(
-                batch_executor.submit(upload_session_appends, client, start_batch_result.session_ids[index], file, args)
+                batch_executor.submit(
+                    upload_session_appends,
+                    client, start_batch_result.session_ids[index], file, args
+                )
             )
 
     for future in futures:
@@ -144,7 +166,8 @@ def upload_files(client, files, args):
     if finish_launch.is_async_job_id():
         logging.info(f"Polling for status of batch of {str(len(entries))} entries...")
         while True:
-            finish_job = client.files_upload_session_finish_batch_check(async_job_id=finish_launch.get_async_job_id())
+            finish_job = client.files_upload_session_finish_batch_check(
+                async_job_id=finish_launch.get_async_job_id())
             if finish_job.is_in_progress():
                 time.sleep(.5)
             else:
@@ -161,7 +184,8 @@ def upload_files(client, files, args):
         if entry.is_success():
             logging.info(f"File successfully uploaded to '{entry.get_success().path_lower}'.")
         elif entry.is_failure():
-            logging.error(f"Commit for path '{entries[index].commit.path}' failed due to: {entry.get_failure()}")
+            logging.error(f"Commit for path '{entries[index].commit.path}'"
+                         f" failed due to: {entry.get_failure()}")
 
     return uploaded_size
 
