337f863c2e9e107046ebbac3db4c8f156f269889
Greg <greg-db@users.noreply.github.com>
2023-08-16 15:51:05 -0400

    Remove references to retired Python 2

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ dist = setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
