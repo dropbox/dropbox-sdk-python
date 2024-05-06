46126c67afa178b78bca2c747a8bdb0190d87bb6
Max Belanger <max@dropbox.com>
2024-05-06 16:07:01 -0700

    refresh support matrix

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ install_reqs = [
     'requests >= 2.16.2',
     'six >= 1.12.0',
     'stone >= 2',
+    'setuptools',
 ]
 
 setup_requires = [
@@ -72,11 +73,11 @@ dist = setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
