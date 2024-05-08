75596daf316b4a806f18057e2797a15bdf83cf6d
Max Bélanger <aeromax@gmail.com>
2024-05-08 15:23:23 -0700

    Restore CI, including Python 2.7 (#492)
    * Restores CI to a green state to aid in landing other changes.
    * Python 2.7 is still covered, but only on Ubuntu and excluding PyPy. Changes are made to `requirements.txt` et al to ensure this works.
    * Runner versions are pinned to avoid automatic breakage, required statuses are updated to match. 
    * Codecov action is updated, token is now explicitly passed. 
    * Fixes an integration test breaking due to backend changes, to be investigated separately.

--- setup.py
+++ setup.py
@@ -25,15 +25,16 @@ for line in open(dbx_mod_path):
 version = eval(line.split('=', 1)[1].strip())  # pylint: disable=eval-used
 
 install_reqs = [
-    'requests >= 2.16.2',
+    'requests<2.30',
+    'urllib3<2',
     'six >= 1.12.0',
-    'stone >= 2',
+    'stone>=2,<3.3.3',
 ]
 
 setup_requires = [
     # Pin pytest-runner to 5.2.0, since 5.3.0 uses `find_namespaces` directive, not supported in
     # Python 2.7
-    'pytest-runner == 5.2.0',
+    'pytest-runner==5.2.0',
 ]
 
 # WARNING: This imposes limitations on test/requirements.txt such that the
