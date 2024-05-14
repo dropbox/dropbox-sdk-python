011303443dfb5ce8243ee8081836948c338b02f2
Max Belanger <max@dropbox.com>
2024-05-13 21:55:49 -0700

    bump copyright

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Dropbox for Python'
-copyright = u'2015-2019, Dropbox, Inc.'
+copyright = u'2015-2024, Dropbox, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
