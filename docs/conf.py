1d379b47b55e945f0bcf2d5d0b59541ec340a3f7
Max Bélanger <max@dropbox.com>
2024-05-14 11:46:21 -0700

    Further fixes to doc generation (#503)

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ import dropbox
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.mathjax',
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -108,7 +109,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
