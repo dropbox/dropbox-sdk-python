8941feff02041a0cb5e0bd1421f12dd16674cb96
Carlos Fuentes <92164482+cfuentes-dbx@users.noreply.github.com>
2025-12-18 18:03:02 +0000

    Adding requirements.txt into MANIFEST.in (#520)
    * Adding requirements.txt into MANIFEST.in
    
    * Updating coverage.yml
    
    * Fixing python version for Integration Coverage
    
    * Updating macos in ci.yml
    
    * Updating Workflows to deprecate all version of pythons & macos
    
    * Updating Workflows to deprecate all version of pythons & macos: ci.yml
    
    * Updating Workflows to deprecate all version of pythons & macos: ci.yml
    
    * Updating Workflows to deprecate all version of pythons & macos: ci.yml - new try
    
    * ci.yml going back to ubuntu 20.04 for 3.7
    
    * ci.yml going back to ubuntu 20.04 for Docs
    
    * Reverting setup-python
    
    * Changing actions to Ubuntu 22.04 as 20.04 is deprecated
    
    * Removing python2.7
    
    * Changing actions versions
    
    * Changing actions versions
    
    * Changing actions versions
    
    * Changing actions versions
    
    * Changing actions versions
    
    * Changing actions versions
    
    * Changing actions versions
    
    * Changing actions versions

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ dist = setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
